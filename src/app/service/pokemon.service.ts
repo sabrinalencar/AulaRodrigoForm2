e96e839465c502a5e49032f77ec6757b0686d395
Rodrigo <rodrigobragavasco@gmail.com>
2019-07-21 01:00:05 -0300

    + função na service pokemon/ fonte global

--- src/app/service/pokemon.service.ts
+++ src/app/service/pokemon.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  pokeApiUrl = 'https://pokeapi.co/api/v2/';
+
+  private httpHeaders = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  };
+
+  constructor(public http: HttpClient) { }
+
+  getPokemon(id): Observable<any> {
+    return this.http.get(this.pokeApiUrl + 'pokemon/' + id, this.httpHeaders).pipe(map(res => res));
+  }
+
 }
